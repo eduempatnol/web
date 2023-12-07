54112f24b25eaf9d6ce9072aa7484909d572dddb
agungd3v <agungd3v@gmail.com>
2023-12-07 20:10:34 +0700

    update

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -83,7 +83,7 @@ class PaymentController extends Controller
             if (!$payment) {
                 throw new \Exception("Error, invoice not found");
             }
-            if (!in_array(["200", "201"], $request->status_code)) {
+            if (!in_array($request->status_code, ["200", "201"])) {
                 throw new \Exception("Error, pay invoice");
             }
 
