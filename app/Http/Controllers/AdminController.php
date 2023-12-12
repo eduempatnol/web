60addc17d86e131c7dcd1e43e0de3b746bfcc60e
agungd3v <agungd3v@gmail.com>
2023-12-12 08:47:41 +0700

    add info transaction admin

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -2,11 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Course;
+use App\Models\CourseInvoice;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class AdminController extends Controller
 {
     public function dashboard() {
-        return view("admin.dashboard");
+        $courses = Course::count();
+        $students = User::where("role_id", 2)->count();
+        $instructors = User::where("role_id", 3)->count();
+        $transaction = CourseInvoice::where("status", "Success")->sum("amount");
+        return view("admin.dashboard", compact("courses", "students", "instructors", "transaction"));
     }
 }
